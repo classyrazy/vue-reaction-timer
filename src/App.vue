b1d7e6888e0ef5040f07573e2a7eed49897e13ec
classyrazy <66884694+classyrazy@users.noreply.github.com>
2021-08-22 22:24:24 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <h1>Vut Reaction Timer</h1>
+  <button @click="start" :disabled="isplaying">Play</button>
+  <Block v-if="isplaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResult" :score="score"/>
+
+  <!-- <p v-if="showResult">Reaction Time: {{ score }}</p> -->
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
+
+export default {
+  name: "App",
+  components: {
+    Block,
+    Result,
+  },
+  data() {
+    return {
+      isplaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      this.isplaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      console.log(this.delay);
+      this.showResult = false;
+    },
+    endGame(reactionTimePassed) {
+      this.score = reactionTimePassed;
+      this.isplaying = false;
+      this.showResult = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  cursor: not-allowed;
+  opacity: 0.2ttttttttttttttttttdhhjdks5tdttttgyttttttt5ttttttttttttgggtttttttgtgft;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="result-con">
+    <h2>This is Your Result</h2>
+    <p>Result Time: {{ score }}ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data () {
+      return {
+          rank: null,
+      }
+  },
+  mounted () {
+      if(this.score < 250){
+          this.rank = "Ninja Fingers"
+      }else if(this.score < 400){
+           this.rank = "Rapid Reflexes"
+      }else{
+          this.rank = "Snail pace"
+      }
+  }
+};
+</script>
+
+<style>
+.result-con {
+  background: #000;
+  color: white;
+  width: 400px;
+  border-radius: 20px;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
